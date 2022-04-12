00c150fecd75531552dc6e0c090a40ea27d33dd9
Dilxxn <103541228+Dilxxn@users.noreply.github.com>
2022-04-12 17:02:18 -0600

    Create config.py

---
+++ .config/qtile/config.py
@@ -0,0 +1,30 @@
+
+### QTILE ###
+
+from settings.keys import keys
+from settings.mouse import mouse
+from settings.groups import groups
+from settings.layouts import layouts, floating_layout
+from settings.widgets import widget_defaults, extension_defaults, screens
+
+dgroups_key_binder = None
+
+dgroups_app_rules = []
+
+follow_mouse_focus = True
+
+bring_front_click = False
+
+cursor_warp = False
+
+auto_fullscreen = True
+
+focus_on_window_activation = "smart"
+
+reconfigure_screens = True
+
+uto_minimize = True
+
+l_input_rules = None
+
+wmname = "LG3D"
