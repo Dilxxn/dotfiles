d2976c11e376696ee297f2dfa07c81f5f18791e8
Dilxxn <103541228+Dilxxn@users.noreply.github.com>
2022-04-12 17:02:44 -0600

    Create colors.py

---
+++ .config/qtile/themes/colors.py
@@ -0,0 +1,10 @@
+
+### QTILE COLORS ###
+
+# Material Ocean
+colors = ["#0f101a","#353c4a",
+          "#f1ffff","#0f101a",
+          "#a151d3","#f1ffff",
+          "#a151d3","#F10717",
+          "#a151d3","#F07178",
+          "#fb9f7f","#ffd47e"]
