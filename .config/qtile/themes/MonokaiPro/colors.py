6dbc43824062560f563c1997ee198be8ba9b8374
Dilxxn <103541228+Dilxxn@users.noreply.github.com>
2022-04-12 17:04:41 -0600

    Create colors.py

---
+++ .config/qtile/themes/MonokaiPro/colors.py
@@ -0,0 +1,10 @@
+
+### QTILE COLORS ###
+
+# MonokaiPro
+colors = ["#2d2a2e","#5a565b",
+          "#ffffff","#2d2a2e",
+          "#a9dc76","#ffffff",
+          "#727072","#ff6188",
+          "#a9dc76","#ffd866",
+          "#ff6188","#ab9df2"]
