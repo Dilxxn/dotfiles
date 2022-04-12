1da32debf90384efe23de40b62fd2326146bc17f
Dilxxn <103541228+Dilxxn@users.noreply.github.com>
2022-04-12 17:03:14 -0600

    Create colors.py

---
+++ .config/qtile/themes/Rosepine/colors.py
@@ -0,0 +1,10 @@
+
+### QTILE COLORS ###
+
+# Rosepine
+colors = ["#1f1d29","#666a72",
+          "#ffffff","#1f1d29",
+          "#eabbb9","#f1ffff",
+          "#4c566a","#ea6f91",
+          "#eabbb9","#f1ca93",
+          "#9bced7","#ea6f91"]
