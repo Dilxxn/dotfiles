43d339414e48e445008fac96720bad2f1f9a23e4
Dilan Rojas <103541228+Dilxxn@users.noreply.github.com>
2022-04-14 11:29:55 -0600

    Create colors.py

---
+++ .config/qtile/themes/DistroTube/colors.py
@@ -0,0 +1,9 @@
+### QTILE COLORS ###
+
+# DistroTube
+colors = ["#272935","#656565",
+          "#ffffff","#ffffff",
+          "#7197e7","#ffffff",
+          "#727072","#ff6188",
+          "#7197e7","#a77ac4",
+          "#7197e7","#a77ac4"]
